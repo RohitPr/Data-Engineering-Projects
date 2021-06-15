3b9d6e401613f2ddb398420d9cc45e76a87048c7
RohitPr <bladekiller95@gmail.com>
2021-06-15 21:59:49 +0530

    push

--- Words_CountRDD/words_count.py
+++ Words_CountRDD/words_count.py
@@ -1,1 +1,16 @@
 from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setMaster("local").setAppName("WordCount")
+sc = SparkContext(conf=conf)
+
+
+file = sc.textFile(
+    "/media/rohit/My Files/Study/Data Engineering Projects/Words_CountRDD/book.txt")
+
+words = file.flatMap(lambda x: x.split())
+words_count = words.countByValue()
+
+for word, count in words_count.items():
+    clean_word = word.encode("ascii", "ignore")
+    if clean_word:
+        print(clean_word, count)
