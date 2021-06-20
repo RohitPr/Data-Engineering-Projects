3fde84435419e5bdb02aa6daff8df5e9f33ea6de
RohitPr <bladekiller95@gmail.com>
2021-06-21 01:16:52 +0530

    Update

---
+++ Word_CountSort_SparkSQL/word_count.py
@@ -0,0 +1,23 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as func
+
+spark = SparkSession.builder.appName("WordCount").getOrCreate()
+
+# Read each line of the  book into DF
+inputDF = spark.read.text(
+    "/media/rohit/My Files/Study/DE Projects/Word_CountSort_SparkSQL/book.txt")
+
+# Split using a regular expression that extracts words and removes spaces
+words = inputDF.select(func.explode(
+    func.split(inputDF.value, "\\W+")).alias("word"))
+
+words.filter(words.word != "")
+
+lowercaseWords = words.select(func.lower(words.word).alias("word"))
+
+wordCounts = lowercaseWords.groupBy("word").count()
+
+wordCountsSorted = wordCounts.sort("count")
+
+# To Show all the results instead of default 20
+wordCountsSorted.show(wordCountsSorted.count())
