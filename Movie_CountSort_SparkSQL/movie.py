9c93c0f6cbb63ceb168f0508f42a7dbac73a421c
RohitPr <bladekiller95@gmail.com>
2021-06-21 03:00:00 +0530

    Update

--- Movie_CountSort_SparkSQL/movie.py
+++ Movie_CountSort_SparkSQL/movie.py
@@ -4,18 +4,10 @@ from pyspark.sql.types import StructType, StructField, IntegerType, LongType
 
 spark = SparkSession.builder.appName("PopularMovies").getOrCreate()
 
-# Create schema
-schema = StructType([
-    StructField("userID", IntegerType(), True),
-    StructField("movieID", IntegerType(), True),
-    StructField("rating", IntegerType(), True),
-    StructField("timestamp", LongType(), True)])
-
-
 moviesDF = spark.read.option("header", "true").option("inferSchema", "true").csv(
     "/media/rohit/My Files/Study/DE Projects/Movie_CountSort_SparkSQL/data/ratings.csv")
 
-topMovieIDs = moviesDF.groupBy("movieID").count().orderBy(func.desc("count"))
+topMovieIDs = moviesDF.groupBy("movieId").count().orderBy(func.desc("count"))
 
 # Top 10 movies
 topMovieIDs.show(10)
