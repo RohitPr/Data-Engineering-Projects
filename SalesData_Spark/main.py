81bcfcf8d46a953b0f84cfbea637c83ea1e2dd59
RohitPr <bladekiller95@gmail.com>
2021-07-02 14:39:27 +0530

    Update

---
+++ SalesData_Spark/main.py
@@ -0,0 +1,30 @@
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as func
+from pyspark.sql.functions import *
+
+
+spark = SparkSession.builder.appName("Sales").getOrCreate()
+
+accounts = spark.read.option("header", "true").option("inferSchema", "true").csv(
+    'data/accounts.csv')
+
+clicks = spark.read.option("header", "true").option("inferSchema", "true").csv(
+    'data/clicks.csv')
+
+products = spark.read.option("header", "true").option("inferSchema", "true").csv(
+    'data/products.csv')
+
+sales_pipeline = spark.read.option("header", "true").option("inferSchema", "true").csv(
+    'data/sales_pipeline.csv')
+
+sales_teams = spark.read.option("header", "true").option("inferSchema", "true").csv(
+    'data/sales_teams.csv')
+
+sales_done_join = sales_teams.join(sales_pipeline, on="sales_agent", how="inner").join(
+    products, on="product", how="inner")
+
+aggregated_data = sales_done_join.where(col("deal_stage") == 'Won').groupBy("manager").agg(func.sum(
+    "sales_price").alias("Grand Total Sales"))
+
+final_data = aggregated_data.select(col("manager").alias(
+    "Manager"), "Grand Total Sales").orderBy("Grand Total Sales", ascending=False).show()
