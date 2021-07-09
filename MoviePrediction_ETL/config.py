9bad850bbcc7a9a6c38d13aea89cce32f0db718a
RohitPr <bladekiller95@gmail.com>
2021-07-10 00:33:34 +0530

    Update

--- MoviePrediction_ETL/config.py
+++ MoviePrediction_ETL/config.py
@@ -1,5 +1,5 @@
-movie_details = {"Luka Chuppi":"2019-03-01","Badla":"2019-03-08","Kesari":"2019-03-21","Gully Boy":"2019-02-14","Total Dhamaal":"2019-02-22"}
+movie_details = {"Luka Chuppi": "2019-03-01", "Badla": "2019-03-08",
+                 "Kesari": "2019-03-21", "Gully Boy": "2019-02-14", "Total Dhamaal": "2019-02-22"}
 
 for key in movie_details:
-    print(key)
-
+    print(key.replace(" ", "-"))
