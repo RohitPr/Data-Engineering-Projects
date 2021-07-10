1526e0b0ab576475367e943572f8293445ae7f42
RohitPr <bladekiller95@gmail.com>
2021-07-10 14:19:24 +0530

    Update

--- MoviePrediction_ETL/config.py
+++ MoviePrediction_ETL/config.py
@@ -1,5 +1,21 @@
-movie_details = {"Luka Chuppi": "2019-03-01", "Badla": "2019-03-08",
-                 "Kesari": "2019-03-21", "Gully Boy": "2019-02-14", "Total Dhamaal": "2019-02-22"}
+from bs4 import BeautifulSoup
+import requests
 
-for key in movie_details:
-    print(key.replace(" ", "-"))
+
+def movie_extract():
+    base_url = "https://boxofficecollection.in/"
+    tail_url = "-box-office-collection-day-wise"
+
+    movie_details = {"Luka Chuppi": "2019-03-01", "Badla": "2019-03-08",
+                     "Kesari": "2019-03-21", "Gully Boy": "2019-02-14", "Total Dhamaal": "2019-02-22"}
+
+    for key in movie_details:
+        movie_name = key.replace(" ", "-")
+        response = requests.get(base_url+movie_name+tail_url)
+        details_page = response.text
+        soup = BeautifulSoup(details_page, "html.parser")
+        data = soup.find("table", {"class": "tablepress"})
+        print(data)
+
+
+movie_extract()
